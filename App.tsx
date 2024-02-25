f6d14551ad3b3fcdda45cdd95890d4c990baefd5
Salem <salemharrathi@gmail.com>
2024-02-25 16:18:46 +0100

    get list of characters

--- App.tsx
+++ App.tsx
@@ -1,23 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper'
+import {StyleSheet, View, Text} from 'react-native';
+import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper'
+import {NavigationContainer} from '@react-navigation/native';
+import Characters from "./components/characters/Characters.components";
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
-  return (
-    <PaperProvider theme={DefaultTheme}>
-      <View style={styles.container}>
-        <Text>Helloo!</Text>
-        <StatusBar style="auto" />
-      </View>
-    </PaperProvider>
-  );
+    return (
+        <PaperProvider theme={DefaultTheme}>
+            <NavigationContainer>
+               <Tab.Navigator style={styles.container}>
+                    <Tab.Screen name="Characters" component={Characters} />
+                    <Tab.Screen name="Locations" component={DetailsScreen} />
+                   <Tab.Screen name="Episodes" component={DetailsScreen} />
+                </Tab.Navigator>
+            </NavigationContainer>
+        </PaperProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        marginTop: 10
+    },
 });
+
+
+function DetailsScreen() {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text>Details Screen</Text>
+        </View>
+        );
+}
---
+++ components/characters/Characters.stylesheet.tsx
@@ -0,0 +1,36 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        alignItems: 'stretch',
+        justifyContent: 'space-around',
+        flexDirection: "column",
+        height: "60%",
+        width: "50%",
+        backgroundColor: 'red'
+    },
+    datatable: {
+        justifyContent: 'space-around'
+    },
+    image: {
+        width: 107,
+        height: 165,
+        padding: 10
+    },
+    row: {
+        dislay: 'flex',
+        width: "100%",
+        justifyContent: 'space-around',
+        backgroundColor: 'red'
+    },
+    cell: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: 30,
+        justifyContent: 'space-around'
+    }
+});
+
+export default styles;
