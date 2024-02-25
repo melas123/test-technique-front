c65278bdaa4fedf403095e572c946d196be9a564
Salem <salemharrathi@gmail.com>
2024-02-25 17:10:13 +0100

    add linting

--- App.tsx
+++ App.tsx
@@ -1,38 +1,37 @@
-import {StyleSheet, View, Text} from 'react-native';
-import {DefaultTheme, Provider as PaperProvider} from 'react-native-paper'
-import {NavigationContainer} from '@react-navigation/native';
-import Characters from "./components/characters/Characters.components";
+import { StyleSheet, View, Text } from 'react-native';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import Characters from './components/characters/Characters.components';
 
 const Tab = createMaterialTopTabNavigator();
 
-export default function App() {
-    return (
-        <PaperProvider theme={DefaultTheme}>
-            <NavigationContainer>
-               <Tab.Navigator style={styles.container}>
-                    <Tab.Screen name="Characters" component={Characters} />
-                    <Tab.Screen name="Locations" component={DetailsScreen} />
-                   <Tab.Screen name="Episodes" component={DetailsScreen} />
-                </Tab.Navigator>
-            </NavigationContainer>
-        </PaperProvider>
-    );
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        marginTop: 10
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: 10,
+  },
 });
 
-
-function DetailsScreen() {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Details Screen</Text>
-        </View>
-        );
+export default function App() {
+  return (
+    <PaperProvider theme={DefaultTheme}>
+      <NavigationContainer>
+        <Tab.Navigator style={styles.container}>
+          <Tab.Screen name="Characters" component={Characters} />
+          <Tab.Screen name="Locations" component={DetailsScreen} />
+          <Tab.Screen name="Episodes" component={DetailsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
 }
